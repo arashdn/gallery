f8f6d5c1aef23d08beab8cd212e9214cc16312a2
sahar <saharrezazadeh96@yahoo.com>
2015-07-02 18:09:05 +0430

    register progressing

--- application/views/register.php
+++ application/views/register.php
@@ -1,13 +1,18 @@
 <div id="container">
 		  <div class="wrapper">
+             
+<div class="col-sm-3" id="ss">
     <form class="form-signin">       
-        <input type="text" class="form-control" name="email" placeholder="Email Address" required="" autofocus="" />
-      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-        <input type="text" class="form-control" name="username" placeholder="username" required="" autofocus="" />
-
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+        <input type="text" class="form-control" name="email" placeholder="ایمیل خود را وارد کنید" required="" autofocus="" />
+        <input type="password" class="form-control" name="password" placeholder="پسوورد خود را وارد کنید" required=""/>      
+        <input type="text" class="form-control" name="username" placeholder="نام کاربری خود را وارد کنید" required="" autofocus="" />
+        
+        
+       <div class="col-sm-5">
+               <button class="btn btn-lg btn-primary btn-block" type="submit" id="sub">ثبت نام</button>   
+        </div>
     </form>
   </div>
     
-
+    </div>
 </div>
