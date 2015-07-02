bdce11ae67b8dd521c64edd454c6c3a0657e9edf
sahar <saharrezazadeh96@yahoo.com>
2015-07-02 17:54:16 +0430

    login progressing

--- application/views/master_view.php
+++ application/views/master_view.php
@@ -5,7 +5,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Gallery</title>
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
+
+	
+     <script src="<?php echo base_url(); ?>assets/js/jquery-1.11.0.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    
 </head>
 <body>
 	<h1>Header</h1>
