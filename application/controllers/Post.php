e33547d6ffec42683d6bb0296e275b281b4d744d
Arashdn <arash@dargahi.org>
2015-07-03 16:16:22 +0430

    post send args done

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -19,8 +19,37 @@ class Post extends CI_Controller
         $categories = $this->Category->getAllCategory();
             
         $data = array('categories' => $categories);
-        $content = $this->load->view('add_post',$data,true);//NULL->data , true is to load into varible
         
-        $this->load->view('master_view',array('content' => $content));
+        
+        $this->load->library('form_validation');
+        
+        if($this->input->post('submit'))
+        {
+
+            $this->form_validation->set_rules('category', 'دسته بندی', 'required|htmlspecialchars');
+            $this->form_validation->set_rules('subject', 'عنوان', 'required|htmlspecialchars');
+            $this->form_validation->set_rules('description', 'توصیف', 'htmlspecialchars');
+
+            $this->form_validation->set_message('required', '%s نباید خالی باشد');
+
+
+            if ($this->form_validation->run() == FALSE)
+            {
+                $content = $this->load->view('add_post',$data,true);//NULL->data , true is to load into varible
+        
+                $this->load->view('master_view',array('content' => $content));
+            }
+            else
+            {
+                echo 'send';
+            }      
+        }
+        else
+        {
+            $content = $this->load->view('add_post',$data,true);//NULL->data , true is to load into varible
+        
+            $this->load->view('master_view',array('content' => $content));
+        }
+ 
     }
 }
--- application/views/add_post.php
+++ application/views/add_post.php
@@ -1,9 +1,21 @@
-<select name = "reg_university" class="form-control">
-    <?php
-    foreach ($categories as $key => $value) 
-    {
-        echo '<option value="'.$value['id'].'">'.$value['title'].'</option>';
-    }
+<form method="post" enctype="multipart/form-data">
+    
+    <div style="color: red;border: #000 solid  2px;">
+        <?php echo validation_errors(); ?>
+    </div>
+    
+    <select name = "category">
+        <?php
+        foreach ($categories as $key => $value) 
+        {
+            echo '<option value="'.$value['id'].'">'.$value['title'].'</option>';
+        }
+        ?>
+    </select>
 
-    ?>
-</select>
+
+    <input type="text" name = "subject" placeholder="subject">
+    <textarea type="text" name="description" placeholder="description"></textarea>
+    <input type="file" name = "pic" >
+    <input type="submit" name="submit">
+</form>
