14ab54a15efbcd7dae5b7a5c362d0d91780f8e06
sahar <saharrezazadeh96@yahoo.com>
2015-07-04 15:01:15 +0430

    homepage...

--- application/views/home.php
+++ application/views/home.php
@@ -106,10 +106,55 @@
                 </div>
             </div>
      </div>
+           
                 
+                
+                
+                
+   <!-- صفحه نمایش پست -->             
+                
+                
+                <!— Button trigger modal —>
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+ Launch demo modal
+</button>
+
+<!— Modal —>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+ <div class="modal-dialog" role="document">
+ <div class="modal-content">
+ <div class="modal-header">
+ <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+ <h4 class="modal-title" id="myModalLabel">نام فرستنده</h4>
+ </div>
+ <div class="modal-body">
+ <img src="#" />
+     <p> توضیحات عکس</p>
+ </div>
+ <div class="modal-footer">
+    
+     <p> نمایش نظرات</p>
+     <input type="text" class="form-control" placeholder="نظر خود را بنویسید">       
+     <button type="button" class="btn btn-primary">ارسال</button>
+     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>   
+         </div>
+         
+ </div>
+ </div>
+ </div>
+</div>
+                
+      <!-- شماره صفحات -->          
+     <div class="pages">
+            
+          <button type="button" class="btn btn-default" id="page">قبلی</button> 
+         <a href="#">. 1</a>
+           <a href="#">. 2 </a>
+          <button type="button" class="btn btn-default" id="page">بعدی</button> 
+            
+            </div>           
                 
     
     </div>
 	
-</div>
 </div>
