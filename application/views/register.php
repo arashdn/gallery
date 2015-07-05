d0f9d3aa189d7c880dacc0591f8e4fb2c93db124
Arashdn <arash@dargahi.org>
2015-07-05 17:23:00 +0430

    started verfication

--- application/views/register.php
+++ application/views/register.php
@@ -2,58 +2,100 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
-	<title>Gallery</title> 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-rtl.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>Gallery</title> 
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-rtl.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
 
-	
-    <script src="<?php echo base_url(); ?>assets/js/jquery-1.11.0.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+        <script src="<?php echo base_url(); ?>assets/js/jquery-1.11.0.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
     </head>
 
 
 
-<body class="pack">
+    <body class="pack">
 
-<div id="container">
-    <div class="wrapper">
-        <div class="col-sm-3" id="ss">
-            <form class="form-signin" method="POST">
-                <div style="color: red;border: #000 solid  2px;">
-                    <?php echo validation_errors(); ?>
-                </div>
-                 <input type="text" class="form-control" name="email" placeholder="ایمیل خود را وارد کنید" required="" autofocus="" />
-                <input type="password" class="form-control" name="password" placeholder="پسوورد خود را وارد کنید" required=""/>    
-                <input type="password" class="form-control" name="password_conf" placeholder="Password Again" required=""/>   
-                <input type="text" class="form-control" name="username" placeholder="نام کاربری خود را وارد کنید" required="" autofocus="" />
-                <div class="form-group" id="captcha_display">
-                    <script type="text/javascript">
+        <div id="container">
+            <div class="wrapper">
+                <div class="col-sm-3" id="ss">
+                    <form class="form-signin" method="POST">
+                        <div style="color: red;border: #000 solid  2px;">
+                            <?php echo validation_errors(); ?>
+                        </div>
+                        <input type="text" class="form-control" name="email" placeholder="ایمیل خود را وارد کنید" required="" autofocus="" />
+                        <input type="password" id="pass" class="form-control" name="password" placeholder="پسوورد خود را وارد کنید" required=""/>    
+                        <input type="password" id="pass_conf" class="form-control" name="password_conf" placeholder="Password Again" required=""/>   
+                        <input id="username" type="text" class="form-control" name="username" placeholder="نام کاربری خود را وارد کنید" required="" autofocus="" />
+                        <div class="form-group" id="captcha_display">
+                            <script type="text/javascript">
 
-                        function createCaptcha()
-                        {
-                            $.get("<?php echo site_url('captcha/') ?>",'',function (data)
-                            {
-                                $('#captcha_display').html(data);
-                            });
-                        }
+                                function createCaptcha()
+                                {
+                                    $.get("<?php echo site_url('captcha/') ?>", '', function(data)
+                                    {
+                                        $('#captcha_display').html(data);
+                                    });
+                                }
 
-                        $(document).ready(function()
-                        {
-                            createCaptcha();
-                        });
-                    </script>
+                                $(document).ready(function()
+                                {
+                                    createCaptcha();
+                                });
+                            </script>
+                        </div>
+                        <input type="text" class="form-control" name="captcha" placeholder="capthcha" required="" autofocus="" />
+
+                        <div class="col-sm-5">
+                            <input type="submit" class="btn btn-lg btn-primary btn-block" id="sub" name="submit" value="ثبت نام" >
+                        </div>
+                    </form>
                 </div>
-                <input type="text" class="form-control" name="captcha" placeholder="capthcha" required="" autofocus="" />
-                
-                <div class="col-sm-5">
-                <input type="submit" class="btn btn-lg btn-primary btn-block" id="sub" name="submit" value="ثبت نام" >
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-</div>
     </body>
+
+    <script>
+    $("#username").blur(function()
+    {
+        $("#username").css('border-width','2px');
+        if($("#username").val().length < 4)
+        {
+            $("#username").css('border-color','red');
+            //$("#username").addClass( "has-error" );
+        }
+        else
+        {
+            $("#username").css('border-color','green');
+        }
+    });
+    
+    $("#pass").blur(matchPassword());
+    $("#pass_conf").blur(matchPassword());
+    
+    function matchPassword()
+    {
+        $("#pass").css('border-width','2px');
+        $("#pass_conf").css('border-width','2px');
+        var p1 = document.getElementById('pass').;
+        var p2 = $("#pass_conf").val();
+        console.log("p1="+p1);
+        if( p1 == p2)
+        {
+            
+            $("#pass").css('border-color','green');
+            $("#pass_conf").css('border-color','green');
+        }
+        else
+        {
+            $("#pass").css('border-color','red');
+            $("#pass_conf").css('border-color','red');
+        }
+    }
+    
+    </script>
+    
+</html>
